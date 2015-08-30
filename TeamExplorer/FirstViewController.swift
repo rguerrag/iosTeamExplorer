5fd751ede363c8fdcc4743e7839c494fff4423c1
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-08-30 08:36:26 -0600

    Initial Commit

---
+++ TeamExplorer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TeamExplorer
+//
+//  Created by Fidel Esteban Morales Cifuentes on 8/30/15.
+//  Copyright (c) 2015 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TeamExplorer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TeamExplorer
+//
+//  Created by Fidel Esteban Morales Cifuentes on 8/30/15.
+//  Copyright (c) 2015 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
