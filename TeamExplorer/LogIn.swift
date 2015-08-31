b020178688f32cc59439b51d61f8343c7dd758bd
rguerrag <raulg_g@hotmail.com>
2015-08-31 10:48:00 -0600

    LogIn was created

---
+++ TeamExplorer/LogIn.swift
@@ -0,0 +1,9 @@
+//
+//  LogIn.swift
+//  TeamExplorer
+//
+//  Created by Raul Guerra on 31/08/15.
+//  Copyright (c) 2015 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import Foundation
